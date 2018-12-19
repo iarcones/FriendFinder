c89bf8a40fb0dfe91d1a9e4d6e52dc142df75a5f
Isabel <iarcones@gmail.com>
2018-12-18 21:50:03 -0800

    Working post surveys

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+var friendArray = [
+    {
+      "name": "Chueca",
+      "photo": "https://hips.hearstapps.com/ell.h-cdn.co/assets/17/30/1600x2403/gettyimages-494227692.jpg?resize=480:*",
+      "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,49 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on friends
+// ===============================================================================
+
+var friendArray = require("../data/friends");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+
+  // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendArray);
+  });
+
+  
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+
+    // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+    friendArray.push(req.body);
+    res.json(true);
+  });
+
+  //------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    friendArray.length = [];
+
+    res.json({ ok: true });
+  });
+};
