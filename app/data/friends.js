241bd7ed4d9648b6e5584d4c08f9808204504995
Isabel <iarcones@gmail.com>
2018-12-19 16:55:28 -0800

    Working, pending adjunsting style modal and input validations

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,85 @@
-
 var friendArray = [
-    {
-      "name": "Chueca",
-      "photo": "https://hips.hearstapps.com/ell.h-cdn.co/assets/17/30/1600x2403/gettyimages-494227692.jpg?resize=480:*",
-      "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-    }
-  ];
-  
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friendArray;
+  {
+    "name": " Jon Snow",
+    "photo": "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2017/08/helen_sloan_-_hbo_photo_43_-_h_2017.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      1,
+      3,
+      1,
+      5,
+      4,
+      4,
+      2
+    ]
+  },
+  {
+    "name": "Tyrion Lannister",
+    "photo": "https://images.immediate.co.uk/volatile/sites/3/2017/10/game-of-thrones-season-5-7c44358.jpg?quality=90&crop=192px,0px,1079px,719px&resize=620,413",
+    "scores": [
+      5,
+      1,
+      3,
+      1,
+      5,
+      1,
+      5,
+      5,
+      2,
+      1
+    ]
+  },
+  {
+    "name": "Daenerys Targaryen",
+    "photo": "https://www.google.com/search?q=game+of+thrones&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiU0dfpnKvfAhWZFzQIHX4fA4MQ_AUIDygC&biw=1280&bih=667#imgrc=minTVWPjHxY0iM:",
+    "scores": [
+      1,
+      1,
+      4,
+      5,
+      4,
+      1,
+      3,
+      5,
+      2,
+      1
+    ]
+  },
+  {
+    "name": "Cersei Lannister",
+    "photo": "https://images.immediate.co.uk/volatile/sites/3/2017/08/promo324259790.png?quality=90&resize=620,413",
+    "scores": [
+      5,
+      2,
+      4,
+      4,
+      1,
+      3,
+      2,
+      2,
+      4,
+      5
+    ]
+  },
+  {
+    "name": "Arya Stark",
+    "photo": "http://images6.fanpop.com/image/photos/39600000/arya-stark-house-stark-39637640-1280-1019.jpg",
+    "scores": [
+      1,
+      1,
+      4,
+      5,
+      2,
+      1,
+      5,
+      4,
+      4,
+      3
+    ]
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,15 +11,16 @@ var friendArray = require("../data/friends");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
 
   // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
+
     res.json(friendArray);
   });
 
-  
+
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
@@ -29,11 +30,38 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
 
+    console.log("app.post")
     // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+    var scoreUser = req.body.scores;
+    console.log(req.body);
+    console.log(scoreUser);
+    console.log(friendArray.length)
+    var arrayDifs = [];
+    var bestMatch = [];
+    for (var i = 0; i < friendArray.length; i++) {
+      var scoreFriends = friendArray[i].scores;
+      console.log("scoreFirends ", scoreFriends);
+      var dif = 0;
+      for (var j = 0; j < friendArray[i].scores.length; j++) {
+        dif += Math.abs(scoreUser[j] - friendArray[i].scores[j]);
+      }
+      if (bestMatch = []){
+        bestMatch = [friendArray[i].name, friendArray[i].photo, dif]
+      }
+      else if (dif < bestMatch[2]){
+        bestMatch = [friendArray[i].name, friendArray[i].photo, dif]
+      }
+
+      console.log(bestMatch)
+    }
+    console.log(bestMatch);
+    console.log("the best match is: ", bestMatch[0])
     friendArray.push(req.body);
-    res.json(true);
+
+    //here return data for the modal
+    res.json(bestMatch);
   });
 
   //------------------------------------------
